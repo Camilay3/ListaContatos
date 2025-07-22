eace3f294fb29075498c9014b84124780f958b1e
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-07-22 20:07:34 -0300

    Contatos
    Criação do componente contatos e exibição dos contatos em json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,15 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './Componentes/container/container.component';
 import { CabecalhoComponent } from './Componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './Componentes/separador/separador.component';
+import { ContatoComponent } from './Componentes/contato/contato.component';
+
+interface Contato {
+  id: number
+  nome: string
+  telefone: string
+}
+
+import agenda from './agenda.json';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +21,20 @@ import { SeparadorComponent } from './Componentes/separador/separador.component'
     CommonModule,
     RouterOutlet, ContainerComponent,
     CabecalhoComponent,
-    SeparadorComponent],
+    SeparadorComponent,
+    ContatoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  contatos: Contato[] = agenda;
+
+  filtrarContatosPorLetraInicial(letra: string) : Contato[] {
+
+    return this.contatos.filter( contato => {
+      return contato.nome.toLowerCase().startsWith(letra);
+    });
+  }
 }
