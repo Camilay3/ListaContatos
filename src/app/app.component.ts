3679b4e47716d10aef1e7bc1826c3725e180a134
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-07-22 21:07:18 -0300

    Formulário #3
    Iniciando esqueleto do formulário de criação de usuário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ interface Contato {
 }
 
 import agenda from './agenda.json';
+import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
 
 @Component({
   selector: 'app-root',
@@ -24,7 +25,8 @@ import agenda from './agenda.json';
     CabecalhoComponent,
     SeparadorComponent,
     ContatoComponent,
-    FormsModule
+    FormularioContatoComponent,
+    FormsModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
