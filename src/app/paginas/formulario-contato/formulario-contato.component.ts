1170f9db697ce82c2062dea0a7cd9cfdf77ce103
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-07-22 21:21:59 -0300

    formGroup #3
    Entendendo o tratamento de inputs e dados

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ContainerComponent } from '../../Componentes/container/container.component';
 import { CommonModule } from '@angular/common';
 import { SeparadorComponent } from '../../Componentes/separador/separador.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -10,10 +11,23 @@ import { SeparadorComponent } from '../../Componentes/separador/separador.compon
     CommonModule,
     ContainerComponent,
     SeparadorComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
 
+  contatoForm!: FormGroup;
+
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl(''),
+      telefone: new FormControl(''),
+      email: new FormControl(''),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('')
+    })
+  }
 }
